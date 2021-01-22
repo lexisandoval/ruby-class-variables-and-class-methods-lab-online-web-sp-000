c33635da4a5f328910e769fd5bf8eaa56377a354
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-22 18:14:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    if !@@artists.include?(@artist)
+    if @@artists.include?(@artist)
       @@artists << @artist
     end
     @@genres << @genre
